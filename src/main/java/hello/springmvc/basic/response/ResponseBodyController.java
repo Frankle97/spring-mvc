3f0f6bffb2194f2a9611c6df2b9acac552c9ffbf
Frankle97 <pairpapa@gmail.com>
2021-11-17 09:18:21 +0900

    Spring MVC

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBody(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().write("OK");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+ 
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+
+}
